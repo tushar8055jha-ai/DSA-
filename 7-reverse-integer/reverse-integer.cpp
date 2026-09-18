76f5e22b13a71020df71a7bf30f5b2d69398ef9a
tushar8055jha-ai <tushar.8055.jha@gmail.com>
2026-09-18 21:49:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (83.27%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            
+            // Check for positive overflow
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7)) {
+                return 0;
+            }
+            // Check for negative overflow
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            rev = rev * 10 + pop;
+        }
+        
+        return rev;
+    }
+};
